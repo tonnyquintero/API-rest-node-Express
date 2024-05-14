66dc78f8497ffc9374fb9d0cc9683af02e9c4cdb
tonny187 <tonyoanthony@gmail.com>
2024-05-14 17:00:26 -0500

    adding express library

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ app.get('/', (req, res) => {
   res.send('Hola mi server en express')
 })
 
+app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Product 1',
+      price: 1000
+    },
+  {
+    name: 'Produc 2',
+    price: 2000
+  }
+  ])
+})
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, desde nueva ruta')
+})
+
 app.listen(port, () => {
   console.log(`Mi port ${port}`);
 })
