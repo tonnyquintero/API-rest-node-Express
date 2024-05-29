0687a4273ccbd6691e466c4a4444f1e020298fa4
tonny187 <tonyoanthony@gmail.com>
2024-05-29 18:03:13 -0500

    fixing cors issues

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const port = 3000
 app.use(express.json())
 
 // INCLUYENDO LOS PUERTOS Y DOMINIOS QUE QUIERO PERMITIRLE ACCESO A MI API
-const whiteList = ['http://localhost:3000', 'http://localhost:8080']
+const whiteList = ['http://localhost:3000/', 'http://localhost:8080', 'http://127.0.0.1:3000/']
 const options = {
   origin: (origin, callback) => {
-    if (whiteList.includes(origin)) {
+    if (whiteList.includes(origin) || !origin) {
       callback(null, true)
     } else {
       callback(new Error('No permitido por configuracion de cors en el api'))
