c10c8f23794768b93f7f7009a083c6ed1a8c4155
tonny187 <tonyoanthony@gmail.com>
2024-06-14 19:11:54 -0500

    adding sequelize

--- api/services/products.js
+++ api/services/products.js
@@ -1,14 +1,13 @@
 const { faker } = require('@faker-js/faker')
 const boom = require('@hapi/boom')
-const pool = require('../libs/postgresPool')
+const sequelize = require('../libs/sequelize')
+const { date } = require('joi')
 
 class ProductsService {
 
   constructor() {
     this.products = []
     this.generate()
-    this.pool = pool;
-    this.pool.on('error', (err) => console.error(err))
   }
 
   async generate() {
@@ -34,8 +33,8 @@ class ProductsService {
   }
   async find() {
     const query = 'SELECT * FROM tasks'
-    const rta = await this.pool.query(query)
-    return rta.rows
+    const [ data ] = await sequelize.query(query)
+    return data
   }
 
   async findOne(id) {
